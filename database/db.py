05e79283a603d995f3b4690620b8b4f286f35936
Brody <45079557+brodyu@users.noreply.github.com>
2023-12-21 16:26:08 -1000

    Testing new architecture

--- database/db.py
+++ database/db.py
@@ -1,5 +1,6 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://user:password@localhost/dbname')
+engine = create_engine(os.environ['RENDER_POSTGRESQL_DATABASE_URL'])
 Session = sessionmaker(bind=engine)
--- tasks.py
+++ tasks.py
@@ -1,9 +1,13 @@
 import os
-import time
+import uuid
+
+from datetime import datetime
 from celery import Celery
 from celery.utils.log import get_task_logger
 from google.cloud import bigquery
 from backtester.engine import BacktestEngine
+from database.db import Session
+from database.models import Backtest
 
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 logger = get_task_logger(__name__)
@@ -26,6 +30,32 @@ def run_backtest(self, params):
                                 spread=params.get("spread", 50))
     
     eval_data, unrealized_results = backtester.run()
+
+    # Save unrealized_results dataframe to BigQuery
+    dataset_id = os.getenv('dataset_id')
+    unrealized_results.to_csv('unrealized_results.csv', index=False)
+
+    client = bigquery.Client()
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        autodetect=True,
+    )
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    table_name = f"{dataset_id}.unrealized_results_{timestamp}"
+    with open('unrealized_results.csv', 'rb') as source_file:
+        job = client.load_table_from_file(source_file, table_name, job_config=job_config)
+    job.result()
+    os.remove('unrealized_results.csv')
+
+    # Save backtest metadata to Postgres database
+    session = Session()
+    new_backtest = Backtest(id=uuid.uuid4(),
+                            task_id=task_id,
+                            bigquery_table=table_name)
+    session.add(new_backtest)
+    session.commit()
 
     return unrealized_results
 
