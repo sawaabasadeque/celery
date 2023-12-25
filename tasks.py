e0405e6af1f308c9db1cd8873ffd9c7133cd65f4
Brody <45079557+brodyu@users.noreply.github.com>
2023-12-24 15:30:06 -1000

    Documentation

--- tasks.py
+++ tasks.py
@@ -21,8 +21,21 @@ class NoDataFoundException(Exception):
 
 @app.task(bind=True)
 def run_backtest(self, params):
+    """
+    Run a backtest using the provided parameters.
+
+    Args:
+        self: The task instance.
+        params (dict): A dictionary containing the backtest parameters.
+
+    Returns:
+        dict: The unrealized results of the backtest.
+
+    """
     task_id = self.request.id
-    logger.info(f"Initializing backtest for task {task_id}")
+    testing = params.get("testing", False)
+
+    logger.info(f"Initializing backtest for task {task_id} w/ testing={testing} ...")
     backtester = BacktestEngine(start_date=params.get("start_date"),
                                 end_date=params.get("end_date"),
                                 sell_strike_method=params.get("sell_strike_method", "percent_under"),
@@ -31,17 +44,38 @@ def run_backtest(self, params):
     
     eval_data, unrealized_results = backtester.run()
 
+    statistics_df = generate_statistics(unrealized_results)
+
     dataset_id = os.getenv('dataset_id')
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     table_name = f"{dataset_id}.unrealized_results_{timestamp}"
 
     # Save unrealized_results to BigQuery and metadata to Postgres
+    if testing:
+        logger.info(f"Testing mode: skipping upload to BigQuery and Postgres")
+        return unrealized_results
     upload_to_bigquery(table_name, unrealized_results, 'unrealized_results.csv')
     save_to_postgres(task_id, table_name)
-
     return unrealized_results
 
+def generate_statistics(unrealized_results):
+
+
 def upload_to_bigquery(table_name, df, file_name):
+    """
+    Uploads a DataFrame to a BigQuery table.
+
+    Args:
+        table_name (str): The name of the BigQuery table.
+        df (pandas.DataFrame): The DataFrame to upload.
+        file_name (str): The name of the temporary CSV file to create.
+
+    Raises:
+        Exception: If the upload to BigQuery fails.
+
+    Returns:
+        None
+    """
     logger.info(f'Uploading {file_name} to BigQuery table {table_name}...')
     df.to_csv(file_name, index=False)
     client = bigquery.Client()
@@ -61,6 +95,17 @@ def upload_to_bigquery(table_name, df, file_name):
         os.remove(file_name)
 
 def save_to_postgres(task_id, table_name):
+    """
+    Save the task to the Postgres backtests table.
+
+    Args:
+        task_id (str): The ID of the task.
+        table_name (str): The name of the table in Postgres.
+
+    Raises:
+        Exception: If there is an error while saving the task to Postgres.
+
+    """
     logger.info(f'Saving task {task_id} to Postgres backtests table...')
     session = Session()
     try:
